11431847039320b4178303bc48733bba7d71a2c9
devienyogiana <devien.yogiana@tokopedia.com>
2022-01-23 21:44:27 -0800

    finished hotel parser

--- code/hotel_parser.rb
+++ code/hotel_parser.rb
@@ -1,9 +1,106 @@
+require 'yaml'
 class HotelParser
   # Implement a method to parse the contents of a YAML file and return
   # an object whose values are accessible using the [] operator or method calls
   #
   # Note: Use of the YAML library is fine (encouraged, even) but please don't
   #       use any third-party gems that enable the required functionality.
+  class HotelRoom
+    @@hotelRoomTable
+    def initialize(table)
+      @@hotelRoomTable = table
+    end
+
+    def amenities
+      return @@hotelRoomTable['amenities']
+    end
+
+    def [](name)
+      return @@hotelRoomTable[name]
+    end
+  end
+
+  class HotelRoomArr
+    @@hotelRoomArrTable
+    def initialize(table)
+      @@hotelRoomArrTable = table
+    end
+
+    def first
+      hrntable = HotelRoom.new(@@hotelRoomArrTable.first)
+      return hrntable
+    end
+
+    def last
+      hrntable = HotelRoom.new(@@hotelRoomArrTable.last)
+      return hrntable
+    end
+  end
+
+  class HotelRoomName
+    @@hotelRoomNameTable
+    def initialize(table)
+      @@hotelRoomNameTable = table
+    end
+
+    def rooms
+      hroom = HotelRoomArr.new(@@hotelRoomNameTable['rooms'])
+      return hroom
+    end
+
+    def name
+      return @@hotelRoomNameTable['name']
+    end
+
+    def [](name)
+      if name == 'name'
+        return @@hotelRoomNameTable[name]
+      else
+        hroom = HotelRoomArr.new(@@hotelRoomNameTable['rooms'])
+        return hroom
+      end
+    end
+  end
+
+  class HotelArr
+    @@hotelArrTable
+    def initialize(table)
+      @@hotelArrTable = table
+    end
+
+    def first
+      hrntable = HotelRoomName.new(@@hotelArrTable.first)
+      return hrntable
+    end
+
+    def last
+      hrntable = HotelRoomName.new(@@hotelArrTable.last)
+      return hrntable
+    end
+  end
+
+  class Hotel
+    @@hotelTable
+
+    def initialize(table)
+      @@hotelTable = table
+    end
+
+    # @@hotelTable['hotels'] returns an array
+    def hotels
+      tb = HotelArr.new(@@hotelTable['hotels'])
+      return tb
+    end
+
+    def [](name)
+      tb = HotelArr.new(@@hotelTable['hotels'])
+      return tb
+    end
+  end
+
   def self.parse(filename)
+    table = YAML.load_file(filename)
+    h = Hotel.new(table)
+    return h
   end
 end
--- spec/hotel_parser_spec.rb
+++ spec/hotel_parser_spec.rb
@@ -1,8 +1,8 @@
-require File.expand_path('code/hotel_parser')
+require File.expand_path('../code/hotel_parser')
 
 describe HotelParser do
   describe '.parse' do
-    subject { HotelParser.parse(File.expand_path('data/hotel.yaml')) }
+    subject { HotelParser.parse(File.expand_path('../data/hotel.yaml')) }
 
     it 'allows accessing via []' do
       expect(subject['hotels'].last['rooms'].first['amenities'].last).to eq 'Pool'
